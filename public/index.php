824a45ed61df102d256b718077a0f202d21e7d97
yosef-agil <yosefagil8@gmail.com>
2021-12-01 20:08:35 +0700

    edit file index.php

--- public/index.php
+++ public/index.php
@@ -47,7 +47,7 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
-
+//ini file
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
