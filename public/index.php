c8ac974f4dc53d79cb6d515b485daad8819f6dbe
tian <tianbaskoro049@gmail.com>
2021-12-01 20:53:20 +0700

    i

--- public/index.php
+++ public/index.php
@@ -48,6 +48,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 //ini file punya rio saja jadi nih tian coba
+//haaaahahah
+
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
