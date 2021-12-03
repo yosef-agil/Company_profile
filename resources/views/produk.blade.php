470fcd2980cef82d2f58a4c77239358e479ab41f
Richard kho <richardkho46@gmail.com>
2021-12-03 20:16:46 +0700

    produk berhasil

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -43,14 +43,143 @@
 
 <!-- content -->
 <div class="container">
-  <div class="row">
-    <div class="col-2">
-    </div>
-    <div class="col-8">
-    </div>
-    <div class="col-2">
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Ades</h5>
+        <p class="card-text">Peluncuran Ades baru dari The Coca-Cola Company 
+          ini menampilkan Ades sebagai air minum dalam kemasan yang Murni, Aman dan Terpercaya, yang dijamin oleh The Coca-Cola Company.</p>
+      </div>
     </div>
   </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Aquarius</h5>
+        <p class="card-text">Aquarius adalah produk The Coca-Cola Company. Diperkenalkan pertama kali di Jepang pada tahun 1980an,
+          kini Aquarius hadir di Indonesia untuk menemanimu beraktifitas berat dan berkeringat.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Coca Cola</h5>
+        <p class="card-text">Coca-Cola adalah minuman ringan paling populer dan paling laris dalam sejarah, serta salah satu merek yang paling dikenal di dunia.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Coca Cola Zero</h5>
+        <p class="card-text">Coke Zero adalah produk terbesar Coca-Cola dalam 22 tahun dan diluncurkan pada tahun 2005, mencapai status miliaran dolar pada tahun 2007. 
+          Coca-Cola Zero menawarkan rasa Coca-Cola yang luar biasa, menyegarkan dan bebas gula.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Coca Cola Light</h5>
+        <p class="card-text">Di Indonesia, Minute Maid pertama kali dipasarkan pada tahun 2008 dengan nama Minute Maid Pulpy Orange.
+           Minuman ini dengan cepat menjadi favorit semua kalangan, dan 
+          kini Minute Maid Pulpy hadir  dengan inovasi baru melalui Minute Maid Aloe Vera White Grape dengan potongan lidah buaya.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Minute Maid Nutriboost</h5>
+        <p class="card-text">Minute Maid Nutriboost pertama kali hadir di Indonesia sejak Mei 2013. Minuman mengandung susu asli dan sari buah bikin kombinasi yang Mix enaxx! Tersedia dalam 4 rasa menarik, jeruk, stroberi, mangga dan apel.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+      <h5 class="card-title">Minute Maid Pulpy</h5>
+        <p class="card-text"> Minuman ini dengan cepat menjadi favorit semua kalangan, dan kini Minute Maid Pulpy hadir  dengan inovasi baru melalui Minute Maid Aloe Vera White Grape dengan potongan lidah buaya.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+      <h5 class="card-title">Minute Maid Refresh</h5>
+        <p class="card-text">Minute Maid Refresh adalah minuman rasa jeruk yang enak, menyegarkan, dan sumber vitamin B3 dan B6.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+      <h5 class="card-title">Frestea</h5>
+        <p class="card-text">Frestea diproduksi di bawah otoritas Pacific Refreshments Pte. Ltd dengan menggunakan standar kualitas tinggi The Coca-ColaCompany, 
+          menggunakan teknologi tinggi dan didukung oleh proses produksi yang baik, demi memastikan bahwa setiap botol Frestea memilki kualitas yang sama. </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+      <h5 class="card-title">Sprite</h5>
+        <p class="card-text">Diperkenalkan pada tahun 1961, Sprite adalah minuman ringan dengan aroma rasa lemon-jeruk terkemuka di dunia. Sprite dijual di lebih dari 190 negara dan peringkat sebagai minuman ringan No. 3 di seluruh dunia.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+      <h5 class="card-title">Fanta</h5>
+        <p class="card-text">Diperkenalkan pada tahun 1940, Fanta adalah merek tertua kedua The Coca-Cola Company dan merek terbesar kedua  di luar Amerika Serikat.
+           Fanta Orange adalah rasa utama dari Fanta. Hampir semua varian rasa buah ada dalam Fanta.
+           Dikonsumsi lebih dari 130 juta kali setiap hari di seluruh dunia, konsumen menyukai Fanta karena rasa buahnya.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+      <h5 class="card-title">A&W</h5>
+        <p class="card-text">Merek minuman jenis root beer yang terutama tersedia di Amerika Serikat dan Kanada. 
+          Ditemukan pada 1919 oleh Roy W. Allen, Allen bermitra dengan Frank Wright pada 1922.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img style=" width:100%;" class="card-img-top" src="{{ ('assets/produk/Ades.png') }}" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Schweppes</h5>
+        <p class="card-text">Schweppes hadir dalam berbagai rasa seperti Air Soda, Rasa Jahe dan Air Tonik yang dapat dinikmati sendiri, atau dikombinasikan dengan varian minuman.</p>
+      </div>
+    </div>
+  </div>
+</div>
 </div>
 	
 </body>
