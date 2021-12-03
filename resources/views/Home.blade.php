4bc3644be67e16acf696130db706a947ee55ad27
yosef-agil <yosefagil8@gmail.com>
2021-12-03 18:13:39 +0700

    add html template each pages

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -3,13 +3,9 @@
 	<link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
   <link href="{{asset('/css/master.css')}}" rel="stylesheet">
 	<script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
-  <style>
-    .navbar{
-      height: 100px;
-    }
-  </style>
 </head>
 <body>
+  <!-- navbar -->
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
@@ -23,29 +19,26 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/">Our Company</a>
+              <a class="nav-link active" aria-current="page" href="/company">Our Company</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link " href="/">Brands</a>
+              <a class="nav-link " href="/brands">Brands</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/">Carrers</a>
+              <a class="nav-link" href="/career">Carrers</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/">News</a>
+              <a class="nav-link" href="/news">News</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/">investor</a>
+              <a class="nav-link" href="/investor">investor</a>
             </li>
-            <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
         </div>
       </div>
     </nav>
   </div>
 
+<!-- content -->
 <div class="container">
   <div class="row">
     <div class="col-2">
---
+++ resources/views/company.blade.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+	<link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/css/master.css')}}" rel="stylesheet">
+	<script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
+</head>
+<body>
+  <!-- navbar -->
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <div class="brand">
+            <a class="navbar-brand" href="#">Coca Cola</a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/company">Our Company</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/brands">Brands</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/career">Carrers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/news">News</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/investor">investor</a>
+            </li>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+<!-- content -->
+<div class="container">
+  <div class="row">
+    <div class="col-2">
+    </div>
+    <div class="col-8">
+    </div>
+    <div class="col-2">
+    </div>
+  </div>
+</div>
+	
+</body>
+</html>
