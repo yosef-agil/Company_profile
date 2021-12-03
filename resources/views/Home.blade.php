8f4d6606e8c1fe7bdabbdf50647ca588526e9005
yosef-agil <yosefagil8@gmail.com>
2021-12-03 18:40:30 +0700

    added font family

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -44,12 +44,7 @@
 <!-- content -->
 <div class="container">
   <div class="row">
-    <div class="col-2">
-    </div>
-    <div class="col-8">
-    </div>
-    <div class="col-2">
-    </div>
+    
   </div>
 </div>
 	
