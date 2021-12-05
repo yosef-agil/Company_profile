d8854f7590facaa6b053d345320359bfdd91ccfc
tian <tianbaskoro049@gmail.com>
2021-12-05 22:52:03 +0700

    update faq isu

--- resources/views/faqisu.blade.php
+++ resources/views/faqisu.blade.php
@@ -58,65 +58,94 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-      1. Apa perbedaan antara Coca-Cola Zero Sugar dan Coca-Cola Light?
+      1. Apakah benar Coca-Cola mengandung kokain?
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>Rasanya berbeda.</strong> Kedua minuman tersebut bebas gula dan bebas kalori. Coca-Cola Zero Sugar memiliki tampilan dan rasa seperti Coca-Cola Classic, sementara Coca-Cola Light memiliki perpaduan rasa berbeda yang memberikannya rasa lebih ringan.
+      <strong>Tidak. </strong>Coca-Cola tidak mengandung kokain atau zat berbahaya lainnya, dan kokain tidak pernah ditambahkan Coca-Cola.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-      2. Apa perbedaan antara Coca-Cola Classic, Coca-Cola Zero Sugar dan Coca-Cola Light?
+      2. Apakah saya bisa menggunakan Coca-Cola sebagai pembersih rumah tangga?
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        Coca-Cola Classic adalah produk kola original dan ikonik kami yang diluncurkan pada tahun 1886. Coca-Cola Zero memiliki tampilan dan rasa seperti Coca-Cola Classic namun memiliki kandungan bebas gula.
-        Coca-Cola Light juga merupakan minuman bebas gula namun memiliki rasa yang lebih ringan.
+      Banyak orang yang selama ini menyebarkan rumor tentang Coca-Cola, karena sifat asamnya, dapat digunakan untuk membersihkan toilet dan baterai mobil yang berkarat, melonggarkan baut berkarat dan menghilangkan bintik-bintik karat dari bemper mobil, menghapus minyak dari pakaian dan membersihkan kabut dari kaca depan.
+
+      Memang benar ada sedikit kandungan asam yang dapat dimakan di Coca-Cola, sama seperti yang ditemukan di banyak makanan dan minuman lain. Dan sangat mungkin bahwa asam yang dapat dimakan ini mampu melakukan hal-hal yang telah dirumorkan ini. Tetapi kami tidak merekomendasikan anda untuk menggunakannya demi tujuan ini. Tetap gunakan produk pembersih biasa.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-     3. Berapa banyak minuman yang dijual The Coca-Cola Company di seluruh dunia setiap harinya?
+     3. Apakah benar anda bisa menggunakan Coca-Cola untuk membersihkan toilet?
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-      Sekitar lebih dari 1.9 miliar porsi minuman kami telah dinikmati di lebih dari 200 negara setiap harinya.
+      Bisa saja, tapi akan lebih baik jika anda menggunakan pembersih toilet. Coca-Cola mengandung asam fosfat yang memberikannya rasa yang tajam, dan asam tersebut dapat membantu membersihkan kotoran. Anda akan mendapatkan efek yang sama jika menggunakan makanan dan minuman berasam lainnya, seperti jus lemon. Akan tetapi, Coca-Cola tidak efektif dalam membunuh kuman, dan terdapat banyak produk yang jauh lebih baik untuk membersihkan toilet.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingFour">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-      4. Apakah The Coca-Cola Company memproduksi minuman yang sama di setiap negara?
+      4. Apakah benar gigi akan larut jika dibiarkan semalam di dalam Coca-Cola?
       </button>
     </h2>
-    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>Tidak.</strong> Sebagian besar produk minuman kami, seperti Coca-Cola Classic, tersedia di lebih dari 200 negara di seluruh dunia. Sejumlah produk minuman kami hanya tersedia di negara-negara tertentu.Kami menyesuaikan produk minuman kami dengan selera lokal. Beberapa rasa lebih populer di satu negara dibandingkan dengan negara lain dan seringkali ada bahan yang berbeda di negara tertentu.
-        Tahukah anda? Semua produk minuman kami di Indonesia dibuat dan dikemas di sini.
+      <strong>Tidak, </strong>Coca-Cola tidak akan melarutkan gigi dalam semalam. Memang benar bahwa makanan atau minuman yang mengandung gula dan bersifat asam, termasuk minuman ringan, dapat berkontribusi pada kerusakan gigi dan erosi, terutama jika Anda sering mengekonsumsinya. Kunci untuk kesehatan gigi yang baik adalah dengan merawat kebersihan gigi yang baik dan menyikat gigi Anda secara teratur. Penting juga untuk memiliki makanan dan minuman secukupnya.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingFive">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-      5. Apakah Coca-Cola memiliki rasa yang berbeda di berbagai negara?
+      5. Benarkah anda bisa meninggal karena makan Mentos dan minum Coke pada saat yang bersamaan?
       </button>
     </h2>
-    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-      Bahan dasar dan proses yang digunakan untuk membuat Coca-Cola sama di semua negara, meskipun cara setiap orang mempersepsikan rasa sangatlah berbeda. Sangat mungkin bagi minuman ringan yang sama memiliki rasa yang sedikit berbeda karena sejumlah faktor lain seperti suhu dimana minuman tersebut dikonsumsi, atau kondisi dimana minuman tersebut disimpan sebelum dikonsumsi.
-      </div>
+      Ini merupakan mitos yang beredar di masyarakat. Memang benar memasukkan Mentos ke dalam botol Coca-Cola /Coca-Cola Light/ minuman berkarbonasi lain akan menyebabkan air mancur soda yang meletus dari botol. Namun, anda tidak akan mendapatkan efek yang sama jika Mengonsumsi Mentos setelah minum Coca-Cola /Coca-Cola Light/ minuman berkarbonasi lain. Letusan yang anda dapatkan ketika mencampur Mentos dan Coca-Cola /Coca-Cola Light / minuman berkarbonasi lain dihasilkan oleh lapisan kasar permen, yang menyebabkan begitu banyak gelembung terbentuk secara cepat di permukaannya saat dimasukkan ke dalam minuman berkarbonasi. Namun, lapisan kasar permen tersebut akan segera larut saat anda mengonsuminya. Jadi, anda tidak akan mendapatkan reaksi yang sama di perut anda. 
+    </div>
     </div>
   </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSix">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+      6. Apakah benar mengonsumsi durian sambil meminum Coke dapat menyebabkan kematian?
+      </button>
+    </h2>
+    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Terima kasih atas pertanyaan anda. Kami dapat memastikan bahwa mengonsumsi durian dan meminum Coca-Cola pada saat yang bersamaan tidak membahayakan kesehatan anda.
+    </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSeven">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+      7. Apa yang terjadi satu jam setelah anda minum sekaleng Coca-Cola? / Apa yang terjadi setelah meminum Coca-Cola?
+      </button>
+    </h2>
+    <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Masyarakat telah menikmati Coca-Cola secara aman selama lebih dari 130 tahun. Coca-Cola memang memiliki kandungan gula dan kafein, dan seperti minuman ringan lainnya, dapat dinikmati dalam jumlah secukupnya sebagai bagian dari gaya hidup yang seimbang. Kami menyediakan berbagai pilihan minuman, termasuk produk rendah gula, bebas gula dan bebas kafein, sehingga anda dapat memilih produk yang tepat sesuai kebutuhan anda.
+    </div>
+    </div>
+  </div>
+
+
+
 </div>
     </div>
     
