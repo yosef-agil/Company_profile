36e5e9eb3bb37a143ddb70d77b14c369a8fe2395
Richard kho <richardkho46@gmail.com>
2021-12-05 22:51:23 +0700

    navbar berhasil

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -17,7 +17,7 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="padding-right: 100px;" >
             <li class="nav-item">
               <a class="nav-link" aria-current="page" href="/home">Home</a>
             </li>
