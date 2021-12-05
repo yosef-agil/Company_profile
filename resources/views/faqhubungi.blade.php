0c355e176c98612c9579bbbfa35e705e0775083d
tian <tianbaskoro049@gmail.com>
2021-12-05 22:26:51 +0700

    update faq page

---
+++ resources/views/faqhubungi.blade.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+	<link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/css/master.css')}}" rel="stylesheet">
+	<script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
+</head>
+<body>
+  <!-- navbar -->
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <div class="brand">
+            <a class="navbar-brand" href="#">Coca Cola</a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/produk">Produk Kami</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/artikel">Artikel</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/tujuan">Tujuan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/tentang">Tentang</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/faq">FAQ</a>
+            </li>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+<!-- content -->
+<div class="container">
+  <div class="row ">
+    <div class="col-2">
+    </div>
+    <div class="col-8" >
+      <h1 class="text-center">Informasi Coca Cola</h1>
+    </div>
+    <div class="col-2">
+    </div>
+
+    <div class="row ">
+    <div class="col-12">
+    <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      1. Apa perbedaan antara Coca-Cola Zero Sugar dan Coca-Cola Light?
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>Rasanya berbeda.</strong> Kedua minuman tersebut bebas gula dan bebas kalori. Coca-Cola Zero Sugar memiliki tampilan dan rasa seperti Coca-Cola Classic, sementara Coca-Cola Light memiliki perpaduan rasa berbeda yang memberikannya rasa lebih ringan.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+      2. Apa perbedaan antara Coca-Cola Classic, Coca-Cola Zero Sugar dan Coca-Cola Light?
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Coca-Cola Classic adalah produk kola original dan ikonik kami yang diluncurkan pada tahun 1886. Coca-Cola Zero memiliki tampilan dan rasa seperti Coca-Cola Classic namun memiliki kandungan bebas gula.
+        Coca-Cola Light juga merupakan minuman bebas gula namun memiliki rasa yang lebih ringan.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+     3. Berapa banyak minuman yang dijual The Coca-Cola Company di seluruh dunia setiap harinya?
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Sekitar lebih dari 1.9 miliar porsi minuman kami telah dinikmati di lebih dari 200 negara setiap harinya.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+      4. Apakah The Coca-Cola Company memproduksi minuman yang sama di setiap negara?
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>Tidak.</strong> Sebagian besar produk minuman kami, seperti Coca-Cola Classic, tersedia di lebih dari 200 negara di seluruh dunia. Sejumlah produk minuman kami hanya tersedia di negara-negara tertentu.Kami menyesuaikan produk minuman kami dengan selera lokal. Beberapa rasa lebih populer di satu negara dibandingkan dengan negara lain dan seringkali ada bahan yang berbeda di negara tertentu.
+        Tahukah anda? Semua produk minuman kami di Indonesia dibuat dan dikemas di sini.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFive">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+      5. Apakah Coca-Cola memiliki rasa yang berbeda di berbagai negara?
+      </button>
+    </h2>
+    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Bahan dasar dan proses yang digunakan untuk membuat Coca-Cola sama di semua negara, meskipun cara setiap orang mempersepsikan rasa sangatlah berbeda. Sangat mungkin bagi minuman ringan yang sama memiliki rasa yang sedikit berbeda karena sejumlah faktor lain seperti suhu dimana minuman tersebut dikonsumsi, atau kondisi dimana minuman tersebut disimpan sebelum dikonsumsi.
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+    
+    
+</div>
+</div>
+	
+</body>
+</html>
---
+++ resources/views/faqkomposisi.blade.php
@@ -0,0 +1,200 @@
+<html>
+<head>
+	<link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/css/master.css')}}" rel="stylesheet">
+	<script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
+</head>
+<body>
+  <!-- navbar -->
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <div class="brand">
+            <a class="navbar-brand" href="#">Coca Cola</a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/produk">Produk Kami</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/artikel">Artikel</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/tujuan">Tujuan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/tentang">Tentang</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/faq">FAQ</a>
+            </li>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+<!-- content -->
+<div class="container">
+  <div class="row ">
+    <div class="col-2">
+    </div>
+    <div class="col-8" >
+      <h1 class="text-center">Informasi Coca Cola</h1>
+    </div>
+    <div class="col-2">
+    </div>
+
+    <div class="row ">
+    <div class="col-12">
+    <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      1. Berapakah kandungan gula yang terdapat di Coca-Cola Classic?
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Sebagai aturan umum, terdapat 10.6g kandungan gula per 100ml Coca-Cola Classic.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+      2. Berapakah kandungan gula yang terdapat di Coca-Cola Light?
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>Tidak ada sama sekali.</strong>Kami menggunakan campuran pemanis rendah kalori untuk menghadirkan rasa Coca-Cola Light yang nikmat dan bebas akan gula.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+     3. Berapakah kandungan kafein yang terdapat di Coca-Cola? 
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>Tidak ada sama sekali.</strong>Jumlah kafein pada kaleng 330ml: Coca-Cola Classic = 32mg,	Coca-Cola Zero Sugar = 32mg,	Coca-Cola Light = 42mg.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+      4. Berapakah kandungan gula yang terdapat di Coca-Cola Zero Sugar? 
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Kami menggunakan campuran pemanis dan perasa rendah kalori untuk membuat rasa Coca-Cola Zero Sugar semakin menyerupai Coca-Cola Classic.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFive">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+      5. Berapakah kandungan gula yang terdapat di Coca-Cola Zero Sugar? 
+      </button>
+    </h2>
+    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>Tidak</strong> Bahan dasar dan proses produksi kami diatur secara ketat oleh pemerintah dan otoritas kesehatan di lebih dari 200 negara. Semua institusi tersebut telah secara konsisten mengakui Coca-Cola sebagai produk non-alkohol.    
+    </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSix">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+      6. Apa saja komposisi Coca-Cola Classic?
+      </button>
+    </h2>
+    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Berikut adalah daftar lengkap bahan dasar Coca-Cola:
+        Air berkarbonasi – Sekitar 90% Coca-Cola adalah air. Bagian yang dikarbonasi adalah karbon dioksida yang dimurnikan, yang memberikan “gelembung” atau “desis” khas Coca-Cola.
+        Gula – Rasa manis Coca-Cola (dan rasa nikmatnya di lidah) berasal dari gula. Coca-Cola Zero Sugar dan Coca-Cola Light tidak memiliki kandungan gula sama sekali, 
+        Warna karamel – Karamel yang sangat spesifik dibuat khusus bagi Coca-Cola untuk memberikan warna minumannya yang khas.
+        Asam fosfat – Rasa tajam Coca-Cola berasal dari penggunaan asam fosfat.
+        Kafein – Rasa sedikit pahit Coca-Cola berasal dari kafein.
+        Konsentrat Kola – Inti dari formula rahasia Coca-Cola adalah perpaduan rasanya yang alami. Ini merupakan bagian formula rahasia yang paling dijaga.
+    </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSeven">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+      7. Why do some Coca-Cola drinks contain caffeine?
+      </button>
+    </h2>
+    <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Banyak orang terkejut saat mengetahui bahwa kafein merupakan salah satu bahan dasar yang memberikan Coca-Cola rasanya yang unik. Kafein adalah bahan yang dikonsumsi oleh banyak orang setiap hari dalam minuman seperti kopi, teh, dan minuman ringan. Kami tahu tidak semua orang ingin mengonsumsi kafein dan tidak semua orang ingin meminumnya setiap saat, jadi kami juga menawarkan berbagai minuman bebas kafein, seperti Fanta dan Sprite, sehingga mereka dapat membuat pilihan minuman yang tepat untuk diri sendiri serta keluarga.
+    </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingEight">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+      8. Apakah minuman dengan pemanis rendah kalori dan tanpa kalori, seperti Coca-Cola Zero Sugar, akan membuat berat badan saya bertambah? 
+      </button>
+    </h2>
+    <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>Tidak</strong> Pemanis rendah dan bebas kalori digunakan sebagai pengganti gula dalam banyak makanan untuk memberikan pilihan kadar dikurangi, rendah, atau tanpa kalori bagi masyarakat. Karena pemanis rendah dan bebas kalori mampu memberikan rasa yang manis dengan jumlah asupan kalori sedikit atau tak ada sama sekali, pemanis tersebut dapat membantu banyak orang dalam mengatur asupan kalori mereka.
+        Semua minuman dapat dinikmati secukupnya sebagai bagian gaya hidup seimbang
+          </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingNine">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+      9. Apakah ada zat aditif yang terkandung di dalam Coca-Cola?
+      </button>
+    </h2>
+    <div id="collapseNine" class="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>Tidak</strong> Kami menggunakan zat aditif dalam sejumlah minuman kami, termasuk Coca-Cola Classic, untuk memberikan rasa dan warna serta meningkatkan daya tarik mereka. Kami hanya menggunakan zat aditif yang diketahui dan telah terbukti aman dalam minuman kami.
+         </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTen">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+      10. Apakah karbonasi di dalam minuman aman untuk dikonsumsi?
+      </button>
+    </h2>
+    <div id="collapseTen" class="accordion-collapse collapse" aria-labelledby="headingTen" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Tentu saja aman. Seperti bahan makanan lain, karbon dioksida, yang digunakan untuk mengkarbonasi minuman, telah ditinjau oleh otoritas yang berwenang di seluruh dunia dan keamanannya telah dipastikan.
+        Karbon dioksida terbentuk secara alami di atmosfer sebagai gas yang tak berwarna dan tak berbau. Kita sendiri mengeluarkan karbon dioksida saat bernafas dan yang diserap oleh tumbuhan.
+    </div>
+    </div>
+  </div>
+
+</div>
+    </div>
+    
+    
+</div>
+</div>
+	
+</body>
+</html>
