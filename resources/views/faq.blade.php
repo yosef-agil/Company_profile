ba04243be7a143329f749a3e98d3f22591383f8c
tian <tianbaskoro049@gmail.com>
2021-12-06 21:00:44 +0700

    up

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -117,6 +117,44 @@
   </div>
 </div>
 </div>
+
+<section class="ftco-section">
+			<div class="container" style="padding-top: 100px;>
+				<div class="row justify-content-center">
+					<div class="col-md-6 text-center">
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="footer-07">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-md-12 text-center">
+						<h2 class="footer-heading">Media Sosial</h2>
+						<p class="menu">
+						  <a class="nav-link " aria-current="page" href="/home">Home</a>
+						  <a class="nav-link " aria-current="page" href="/home">Home</a>
+							<a class="nav-link " aria-current="page" href="/home">Home</a>
+							<a class="nav-link " aria-current="page" href="/home">Home</a>
+							<a class="nav-link " aria-current="page" href="/home">Home</a>
+							<a class="nav-link " aria-current="page" href="/home">Home</a>
+						</p>
+						<ul class="ftco-footer-social p-0">
+              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><span class="ion-logo-twitter"></span></a></li>
+              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="ion-logo-facebook"></span></a></li>
+              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="ion-logo-instagram"></span></a></li>
+            </ul>
+					</div>
+				</div>
+				<div class="row mt-5">
+					<div class="col-md-12 text-center">
+						<p class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ion-ios-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+					  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+					</div>
+				</div>
+			</div>
+		</footer>
 	
 </body>
 </html>
