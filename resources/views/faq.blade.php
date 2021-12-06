93534fb3f4c8ea5b286380cdac667a4e29c6f2e3
tian <tianbaskoro049@gmail.com>
2021-12-06 22:35:09 +0700

    up faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -48,7 +48,7 @@
     <div class="col-2">
     </div>
     <div class="col-8" >
-      <h1 class="text-center">Informasi Coca Cola</h1>
+      <h1 class="text-center">Informasi Coca Cola Company</h1>
     </div>
     <div class="col-2">
     </div>
