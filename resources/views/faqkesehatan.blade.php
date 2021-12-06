3bfc4c84438ea3264d6041826c5289752707312f
tian <tianbaskoro049@gmail.com>
2021-12-06 22:46:42 +0700

    up faq

--- resources/views/faqkesehatan.blade.php
+++ resources/views/faqkesehatan.blade.php
@@ -7,19 +7,19 @@
 <body>
   <!-- navbar -->
   <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
       <div class="container-fluid">
         <div class="brand">
-            <a class="navbar-brand" href="#">Coca Cola</a>
+           <a class="navbar-brand" href="/home" style="padding-left: 50px;">Coca Cola</a>
         </div>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="padding-right: 100px;" >
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/home">Home</a>
+              <a class="nav-link " aria-current="page" href="/home">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link " href="/produk">Produk Kami</a>
@@ -34,7 +34,7 @@
               <a class="nav-link" href="/tentang">Tentang</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/faq">FAQ</a>
+              <a class="nav-link active" href="/faq">FAQ</a>
             </li>
         </div>
       </div>
