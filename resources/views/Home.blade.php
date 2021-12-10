8cb5acdb79bd0a2f6b3c53b2d26052491681ea49
yosef-agil <yosefagil8@gmail.com>
2021-12-10 14:12:40 +0700

    settings on home page

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -9,19 +9,20 @@
 <body>
   <!-- navbar -->
   <div class="container">
-  <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
+    <div class="row">
+       <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
       <div class="container-fluid">
         <div class="brand">
-          <a class="navbar-brand" href="/home" style="padding-left: 50px;">Coca Cola</a>
+          <a class="navbar-brand" href="/home">Coca Cola</a>
         </div>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="padding-right: 100px;" >
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0" >
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/home">Home</a>
+              <a class="nav-link" aria-current="page" href="/home">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link " href="/produk">Produk Kami</a>
@@ -30,7 +31,7 @@
               <a class="nav-link" href="/artikel">Artikel</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/kontak">Kontak Kami</a>
+              <a class="nav-link active" href="/kontak">Kontak Kami</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/tentang">Tentang</a>
@@ -41,43 +42,42 @@
         </div>
       </div>
     </nav>
+    </div>
   </div>
 
 
 <!-- content -->
-<div class="container" style="padding-top: 50px;">
-  <div class="row align-self-center" style="margin:110px 8px 110px 8px;">
+  <div class="container" style="padding-top: 50px;">
+    <div class="row align-self-center" style="margin:100px 0px 100px 0px;">
 
-     <!-- Heading & title -->
-      <div class="col-md-6">
-        <h2>Gandeng Anak-anak muda, Cola-cola Luncurkan Program Plastic Reborn 2.0</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Fuga ab fugit alias ut. Repellendus esse inventore id nihil 
-          accusantium a exercitationem quo libero, amet eaque.
-        </p>
-        <button class="btn btn-primary"><a href="/"></a>Baca lagi</button>
-      </div>
+      <!-- Heading & title -->
+        <div class="col-md-6">
+          <h2>Gandeng Anak-anak muda, Cola-cola Luncurkan Program Plastic Reborn 2.0</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fuga ab fugit alias ut. Repellendus esse inventore id nihil 
+            accusantium a exercitationem quo libero, amet eaque.
+          </p>
+          <button class="btn btn-primary"><a href="/"></a>Baca lagi</button>
+        </div>
 
-      <!-- image -->
-      <div class="col-md-6">
-        <img style=" width:100%;" src="{{ ('assets/beranda/img1-beranda.png') }}" alt="">
-      </div>
+        <!-- image -->
+        <div class="col-md-6">
+          <img style=" width:100%;" src="{{ ('assets/beranda/img1-beranda.png') }}" alt="">
+        </div>
+    </div>
+  </div>
 
-<section class="ftco-section">
-			<div class="container" style="padding-top: 50px;">
-          <div class="row ">
-              <div class="col-12" >
-                <hr size="5px">
-                </div>
-             </div>
-				</div>
-			</div>
-		</section>
+<!-- content 2 -->
+
+
+
+<!-- Footer -->
 		<footer class="footer-07">
+      <hr>
 			<div class="container"  style="padding-top: 20px; ">
 				<div class="row justify-content-center">
-					<div class="col-md-12 text-left">
+					<div class="col-md-12 text-center">
 						<h2 class="footer-heading">Coca Cola Company</h2>
 						<p class="menu">
 						  <a  aria-current="page" style="text-decoration:none ;color: black" href="/home">Home</a> |
