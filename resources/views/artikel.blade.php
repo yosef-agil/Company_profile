9e0877b1408d99d34e5ef21ccbba7efd1eca858b
Richard kho <richardkho46@gmail.com>
2021-12-12 21:59:26 +0700

    ubah navbar footer

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -11,8 +11,8 @@
 
 <body>
     <!-- navbar -->
-    <div class="container-fluid">
-        <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="height: 80px; background-color: #333333">
+<div class="container-fluid">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="height: 80px; background-color: #191919">
             <div class="container-fluid">
                 <div class="brand">
                     <a class="navbar-brand" href="/home" style="padding-left: 40px;"><img style=" width:30%;" src="{{ ('assets/beranda/3.png') }}" alt=""></a>
@@ -26,7 +26,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent"  style="padding-left: 300px; padding-right:40px;">
                     <ul class="navbar-nav  ms-auto ">
                         <li class="nav-item">
-                            <a class="nav-link " style="color: white" aria-current="page" href="/home">Home</a>
+                            <a class="nav-link active" style="color: white" aria-current="page" href="/home">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" style="color: white" href="/produk">Produk</a>
@@ -120,7 +120,7 @@
         </div>
     </div>
 
-    <div class="container-fluid bg-light" style="padding-top: 50px;">
+    <div class="container-fluid" style="padding-top: 50px; background-color: #191919 ; color: white;">
         <div class="row">
             <div class="col-12">
                 <footer class="footer-07">
@@ -129,17 +129,17 @@
                             <div class="col-md-12 text-center">
                                 <h2 class="footer-heading">Coca Cola Company</h2>
                                 <p class="menu">
-                                    <a aria-current="page" style="text-decoration:none ;color: black"
+                                    <a aria-current="page" style="text-decoration:none ;color: white"
                                         href="/home">Home</a> |
-                                    <a aria-current="page" style="text-decoration:none ;color: black"
+                                    <a aria-current="page" style="text-decoration:none ;color: white"
                                         href="/produk">Produk Kami</a> |
-                                    <a aria-current="page" style="text-decoration:none ;color: black"
+                                    <a aria-current="page" style="text-decoration:none ;color: white"
                                         href="/artikel">Artikel</a> |
-                                    <a aria-current="page" style="text-decoration:none ;color: black"
+                                    <a aria-current="page" style="text-decoration:none ;color: white"
                                         href="/kontak">Kontak Kami</a> |
-                                    <a aria-current="page" style="text-decoration:none ;color: black"
+                                    <a aria-current="page" style="text-decoration:none ;color: white"
                                         href="/tentang">Tentang</a> |
-                                    <a aria-current="page" style="text-decoration:none ;color: black"
+                                    <a aria-current="page" style="text-decoration:none ;color: white"
                                         href="/faq">FAQ</a>
                                 </p>
                                 <a href="https://www.facebook.com/Coca-Cola/" class="fa fa-facebook"
