11325bf87355dbd987e5636e035a331e9a3a0e7b
yosef-agil <yosefagil8@gmail.com>
2021-12-12 23:28:34 +0700

    design login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,28 +4,45 @@
     <link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('/css/master.css')}}" rel="stylesheet">
     <script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
+
+    <style>
+      body{
+        background-color:#191919;
+      }
+      .log-admin{
+        background-color:#fff;
+        padding:40px;
+        border-radius:15px;
+        box-shadow: 1px 5px 22px 0px rgba(58, 63, 78, 0);
+      }
+      .pass{
+        padding-top:15px;
+      }
+    </style>
 </head>
 
 <body>
 
     <div class="container">
         <div class="row">
-            <div class="col-2">
-            </div>
-            <div class="col-8">
+            <div class="col-4 position-absolute top-50 start-50 translate-middle">
+                <div class="log-admin">
                 <form method="POST" action="/admincek">
                     {{ csrf_field() }}
-                    <label for="exampleFormControlInput1" class="form-label">UserName</label>
-                    <input type="text" class="form-control" name="nama" id="exampleFormControlInput1"
-                        placeholder="UserName">
-                        
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                    <input type="password" class="form-control" name="password" id="inputPassword"
-                        placeholder="Password">
+                    <h3 style="text-align:center;">Log In - Admin</h3><br>
+                    <hr>
+                    <div class="col-md-12">
+                        <label for="formGroupExampleInput" class="form-label">Email</label>
+                        <input type="text" class="form-control" name="email" id="formGroupExampleInput" placeholder="Your email address..">
+                    </div>
+                    <div class="pass col-md-12">
+                        <label for="formGroupExampleInput2" class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password" id="formGroupExampleInput2" placeholder="Input your password..">
+                    </div>
+                    <br>
 
-                    <button type="submit" value="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" value="submit" class="btn btn-primary" style="width:100%;">Submit</button>
                 </form>
-                <div class="col-2">
                 </div>
             </div>
         </div>
