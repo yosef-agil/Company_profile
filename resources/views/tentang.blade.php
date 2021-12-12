24ab0fad2bd180508a85e0f976f67d7fed3948f3
tian <tianbaskoro049@gmail.com>
2021-12-12 20:20:57 +0700

    UP TENTANG

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -14,7 +14,8 @@
         <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="height: 80px; background-color: #333333">
             <div class="container-fluid">
                 <div class="brand">
-                    <a class="navbar-brand" href="/home" style="padding-left: 40px;"><img style=" width:30%;" src="{{ ('assets/beranda/3.png') }}" alt=""></a>
+                    <a class="navbar-brand" href="/home" style="padding-left: 40px;"><img style=" width:30%;"
+                            src="{{ ('assets/beranda/3.png') }}" alt=""></a>
                 </div>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -22,7 +23,8 @@
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent"  style="padding-left: 300px; padding-right:40px;">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent"
+                    style="padding-left: 300px; padding-right:40px;">
                     <ul class="navbar-nav  ms-auto ">
                         <li class="nav-item">
                             <a class="nav-link " style="color: white" aria-current="page" href="/home">Home</a>
@@ -70,99 +72,125 @@
             </div>
             <div class="col-8">
                 <p>
-                    Dunia terus berubah di sekitar kita. Untuk dapat terus berkembang sebagai bisnis selama sepuluh tahun ke
-                depan dan seterusnya, kami harus melihat ke depan, memahami berbagai tren dan faktor yang akan membentuk
-                bisnis kami di masa depan serta bergerak cepat untuk mempersiapkan diri demi menghadapi tantangan yang
-                akan datang. Kami harus bersiap untuk hari esok mulai hari ini. Itulah Visi 2020 kami. Visi ini
-                menciptakan sebuah tujuan jangka panjang bagi bisnis kami dan memberikan kami sebuah "Peta Jalan" untuk
-                meraih kesuksesan bersama para mitra pembotolan kami.
+                    Dunia terus berubah di sekitar kita. Untuk dapat terus berkembang sebagai bisnis selama sepuluh
+                    tahun ke
+                    depan dan seterusnya, kami harus melihat ke depan, memahami berbagai tren dan faktor yang akan
+                    membentuk
+                    bisnis kami di masa depan serta bergerak cepat untuk mempersiapkan diri demi menghadapi tantangan
+                    yang
+                    akan datang. Kami harus bersiap untuk hari esok mulai hari ini. Itulah Visi 2020 kami. Visi ini
+                    menciptakan sebuah tujuan jangka panjang bagi bisnis kami dan memberikan kami sebuah "Peta Jalan"
+                    untuk
+                    meraih kesuksesan bersama para mitra pembotolan kami.
                 </p>
                 <h5 style="padding-top: 15px; "><strong>Misi Kami</strong></h5>
 
                 <p>
-                Peta Jalan kami dimulai dengan misi kami untuk terus bertahan. Misi ini menegaskan tujuan kami sebagai
-                sebuah perusahaan dan berfungsi sebagai standar yang menjadi tolok ukur seluruh tindakan dan keputusan
-                kami.
+                    Peta Jalan kami dimulai dengan misi kami untuk terus bertahan. Misi ini menegaskan tujuan kami
+                    sebagai
+                    sebuah perusahaan dan berfungsi sebagai standar yang menjadi tolok ukur seluruh tindakan dan
+                    keputusan
+                    kami.
                 </p>
-
-                <li>Untuk menyegarkan dunia...</li>
-                <li>Untuk menginspirasi momen penuh optimisme dan kebahagiaan...</li>
-                <li>Untuk menciptakan nilai dan membuat perbedaan.</li>
-
+                <ul>
+                    <li>Untuk menyegarkan dunia...</li>
+                    <li>Untuk menginspirasi momen penuh optimisme dan kebahagiaan...</li>
+                    <li>Untuk menciptakan nilai dan membuat perbedaan.</li>
+                </ul>
                 <h5 style="padding-top: 15px; "><strong>Visi Kami</strong></h5>
                 <p>
-                Visi kami membentuk kerangka kerja Peta Jalan kami dan memandu setiap aspek dari bisnis kami dengan
-                menjelaskan target yang perlu kami capai untuk terus meraih pertumbuhan yang berkelanjutan dan
-                berkualitas.
-                
-                <div style="padding-top: 5px; ">
-                
-                    <li><strong>SDM: </strong> Menjadi tempat kerja yang baik dimana orang dapat terinspirasi untuk
-                        memenuhi potensi mereka.</li>
-                    <li><strong>Portofolio: </strong> Menghadirkan portofolio merek minuman berkualitas yang mampu
-                        mengantisipasi dan memuaskan keinginan dan kebutuhan orang ke seluruh dunia.</li>
-                    <li><strong>Mitra: </strong> Menjaga jaringan pelanggan dan pemasok yang unggul, bersama menciptakan
-                        nilai bersama yang mampu bertahan lama.</li>
-                    <li><strong>Planet: </strong> Menjadi warga negara yang bertanggung jawab yang membuat perbedaan
-                        dengan membantu membangun dan mendukung masyarakat yang berkelanjutan.</li>
-                    <li><strong>Keuntungan: </strong> Memaksimalkan pengembalian jangka panjang kepada pemilik saham
-                        dengan memperhatikan tanggung jawab secara keseluruhan.</li>
-                    <li><strong>Produktivitas: </strong> Menjadi organisasi yang sangat efektif, terstruktur, dan
-                        bergerak cepat.</li>
-                        
-                </div>
+                    Visi kami membentuk kerangka kerja Peta Jalan kami dan memandu setiap aspek dari bisnis kami dengan
+                    menjelaskan target yang perlu kami capai untuk terus meraih pertumbuhan yang berkelanjutan dan
+                    berkualitas.
+
+                    <div style="padding-top: 5px; ">
+                        <ul>
+                            <li><strong>SDM: </strong> Menjadi tempat kerja yang baik dimana orang dapat terinspirasi
+                                untuk
+                                memenuhi potensi mereka.</li>
+                            <li><strong>Portofolio: </strong> Menghadirkan portofolio merek minuman berkualitas yang
+                                mampu
+                                mengantisipasi dan memuaskan keinginan dan kebutuhan orang ke seluruh dunia.</li>
+                            <li><strong>Mitra: </strong> Menjaga jaringan pelanggan dan pemasok yang unggul, bersama
+                                menciptakan
+                                nilai bersama yang mampu bertahan lama.</li>
+                            <li><strong>Planet: </strong> Menjadi warga negara yang bertanggung jawab yang membuat
+                                perbedaan
+                                dengan membantu membangun dan mendukung masyarakat yang berkelanjutan.</li>
+                            <li><strong>Keuntungan: </strong> Memaksimalkan pengembalian jangka panjang kepada pemilik
+                                saham
+                                dengan memperhatikan tanggung jawab secara keseluruhan.</li>
+                            <li><strong>Produktivitas: </strong> Menjadi organisasi yang sangat efektif, terstruktur,
+                                dan
+                                bergerak cepat.</li>
+                        </ul>
+                    </div>
                 </p>
 
                 <h5 style="padding-top: 15px; "><strong>Budaya Kesuksesan Kami</strong></h5>
                 <p>
-                Budaya Kesuksesan Kami mempertegas segala sikap dan perilaku yang diperlukan untuk menjadikan Visi 2020
-                kami sebuah kenyataan.
+                    Budaya Kesuksesan Kami mempertegas segala sikap dan perilaku yang diperlukan untuk menjadikan Visi
+                    2020
+                    kami sebuah kenyataan.
                 </p>
 
                 <h5 style="padding-top: 15px; "><strong>Mewujudkan Nilai Perusahaan</strong></h5>
                 <p>
-                Nilai-nilai perusahaan kami berfungsi sebagai kompas atas tindakan kami dan mengilustrasikan cara kami
-                berperilaku di seluruh dunia.
-                
+                    Nilai-nilai perusahaan kami berfungsi sebagai kompas atas tindakan kami dan mengilustrasikan cara
+                    kami
+                    berperilaku di seluruh dunia.
 
-                <div style="padding-top: 5px; ">
-                    <li><strong>Kepemimpinan: </strong> Berani untuk membentuk masa depan yang lebih baik</li>
-                    <li><strong>Kolaborasi: </strong> Memanfaatkan kecerdasan kolektif</li>
-                    <li><strong>Integritas: </strong> Jujur kepada diri sendiri dan orang lain</li>
-                    <li><strong>Akuntabilitas: </strong> Jika suatu hal terjadi, saya harus bertanggung jawab</li>
-                    <li><strong>Semangat: </strong> Berkomitmen dalam hati dan pikiran.</li>
-                    <li><strong>Keanekaragaman: </strong> Selalu bersikap inklusif seperti halnya merek kami</li>
-                    <li><strong>Kualitas: </strong> Melakukan yang terbaik dalam semua yang kami lakukan</li>
-                </div>
+                    <div style="padding-top: 5px;">
+                        <ul>
+                            <li><strong>Kepemimpinan: </strong> Berani untuk membentuk masa depan yang lebih baik</li>
+                            <li><strong>Kolaborasi: </strong> Memanfaatkan kecerdasan kolektif</li>
+                            <li><strong>Integritas: </strong> Jujur kepada diri sendiri dan orang lain</li>
+                            <li><strong>Akuntabilitas: </strong> Jika suatu hal terjadi, saya harus bertanggung jawab
+                            </li>
+                            <li><strong>Semangat: </strong> Berkomitmen dalam hati dan pikiran.</li>
+                            <li><strong>Keanekaragaman: </strong> Selalu bersikap inklusif seperti halnya merek kami
+                            </li>
+                            <li><strong>Kualitas: </strong> Melakukan yang terbaik dalam semua yang kami lakukan</li>
+                        </ul>
+                    </div>
                 </p>
 
                 <h5 style="padding-top: 15px; "><strong>Fokus pada Pasar</strong></h5>
                 <p>
-                <li>Fokus pada kebutuhan konsumen, pelanggan dan mitra waralaba kami</li>
-                <li>Terjun ke pasar untuk mendengarkan, mengamati, dan mempelajari</li>
-                <li>Memiliki pandangan global</li>
-                <li>Fokus pada eksekusi pasar setiap harinya</li>
-                <li>Bersikap selalu ingin tahu</li>
+                    <ul>
+                        <li>Fokus pada kebutuhan konsumen, pelanggan dan mitra waralaba kami</li>
+                        <li>Terjun ke pasar untuk mendengarkan, mengamati, dan mempelajari</li>
+                        <li>Memiliki pandangan global</li>
+                        <li>Fokus pada eksekusi pasar setiap harinya</li>
+                        <li>Bersikap selalu ingin tahu</li>
+                    </ul>
                 </p>
 
                 <h5 style="padding-top: 15px; "><strong>Bekerja Cerdas</strong></h5>
                 <p>
-                <li>Bertindak dengan urgensi</li>
-                <li>Tetap responsif terhadap perubahan</li>
-                <li>Memiliki keberanian untuk mengubah arah jika dibutuhkan</li>
-                <li>Tak mudah puas secara konstruktif</li>
-                <li>Bekerja secara efisien</li>
+                    <ul>
+                        <li>Bertindak dengan urgensi</li>
+                        <li>Tetap responsif terhadap perubahan</li>
+                        <li>Memiliki keberanian untuk mengubah arah jika dibutuhkan</li>
+                        <li>Tak mudah puas secara konstruktif</li>
+                        <li>Bekerja secara efisien</li>
+                    </ul>
                 </p>
                 <h5 style="padding-top: 15px; "><strong>Bertindak Seperti Pemimpin</strong></h5>
                 <p>
-                <li>Bertanggung jawab atas semua tindakan yang dilakukan dan tidak dilakukan</li>
-                <li>Mempertahankan aset sistem dan fokus pada pembangunan nilai</li>
-                <li>Memberikan penghargaan kepada pegawai karena telah mengambil risiko dan menemukan cara yang lebih
-                    baik untuk memecahkan masalah</li>
-                    </p>
+                    <ul>
+                        <li>Bertanggung jawab atas semua tindakan yang dilakukan dan tidak dilakukan</li>
+                        <li>Mempertahankan aset sistem dan fokus pada pembangunan nilai</li>
+                        <li>Memberikan penghargaan kepada pegawai karena telah mengambil risiko dan menemukan cara yang
+                            lebih
+                            baik untuk memecahkan masalah</li>
+                    </ul>
+                </p>
                 <h5 style="padding-top: 15px; "><strong>Memaknai Merek</strong></h5>
                 <p>
-                <li>Menginspirasi kreativitas, semangat, optimisme dan kebahagiaan</li>
+                    <ul>
+                        <li>Menginspirasi kreativitas, semangat, optimisme dan kebahagiaan</li>
+                    </ul>
                 </p>
             </div>
             <div class="col-2">
@@ -173,7 +201,8 @@
 
     </div>
 
-    <div class="container-fluid" style="padding-top: 50px; padding-bottom: 40px; background-color: #333333 ; color: white;">
+    <div class="container-fluid"
+        style="padding-top: 50px; padding-bottom: 40px; background-color: #333333 ; color: white;">
         <div class="row">
             <div class="col-12">
                 <footer class="footer-07">
